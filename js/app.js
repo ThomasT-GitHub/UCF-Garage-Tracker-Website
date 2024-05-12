c05541001cc4f71695610af480652f4a8e059647
Thomas Trivino <163817742+thomastrivino@users.noreply.github.com>
2024-05-12 18:20:01 -0400

    Cleaned up code

--- js/app.js
+++ js/app.js
@@ -8,6 +8,47 @@ const getGarageInfo = async () => {
   return result;
 }
 
+(async() => {
+  // Gets our percent bars; garage icons; the hover text for each garage; the loading wheels
+  const percentBars = await document.querySelectorAll(".percent-bar-a,.percent-bar-b,.percent-bar-c,.percent-bar-d,.percent-bar-h,.percent-bar-i");
+  const garages = await document.querySelectorAll(".garage");
+  const garageHoverText = await document.querySelectorAll("#aText,#bText,#cText,#dText,#hText,#iText");
+  const loadingWheels = await document.querySelectorAll(".wheelLoading");
+
+  // Gets the JSON info of the garage data
+  const garageInfo = await getGarageInfo();
+
+  // Hides the loading wheels when data is received
+  loadingWheels.forEach((wheel) => wheel.style.visibility = 'hidden');
+
+  // Keeps track of our position in the percentBars array
+  let pos = 0;
+
+  // For each garage, makes the calculation for the percentage and adds it to the array
+  for (let i = 3; i < 24; i += 4) {
+    // Stores the percentage and available spots
+    let percent = garageInfo[i];
+    let avail = garageInfo[i - 2];
+
+    // If percent is greater than 0 make it visible
+    if (parseInt(percent) > 0) {
+      percentBars[pos].firstElementChild.style.visibility = 'visible';
+    }
+
+    // Sets the percent bars to their width and color
+    percentBars[pos].style.width = percent;
+    percentBars[pos].firstElementChild.innerText = percent;
+    percentBars[pos].style.background = getBarColorFromPercent(percent);
+
+    // Sets the garage icons to their color and sets their hover text
+    garages[pos].style.background = getGarageColorFromPercent(percent);
+    garageHoverText[pos].innerText = avail;
+
+    // Moves to the next garage
+    pos++;
+  }
+})();
+
 // Assigns a color to a percent value for the percent bars and returns the color
 const getBarColorFromPercent = (percent) => {
   // Parses the percent into an int
@@ -43,42 +84,3 @@ const getGarageColorFromPercent = (percent) => {
   else
     return '#9dd298';
 }
-
-(async() => {
-  // Gets our percent bars; garage icons; the hover text for each garage; the loading wheels
-  const percentBars = await document.querySelectorAll(".percent-bar-a,.percent-bar-b,.percent-bar-c,.percent-bar-d,.percent-bar-h,.percent-bar-i");
-  const garages = await document.querySelectorAll(".garage");
-  const garageHoverText = await document.querySelectorAll("#aText,#bText,#cText,#dText,#hText,#iText");
-  const loadingWheels = await document.querySelectorAll(".wheelLoading");
-
-  // Gets the JSON info of the garage data
-  let garageInfo = await getGarageInfo();
-
-  // Hides the loading wheels when data is received
-  loadingWheels.forEach((wheel) => wheel.style.visibility = 'hidden');
-
-  // Keeps track of our position in the percentBars array
-  let pos = 0;
-
-  // For each garage, makes the calculation for the percentage and adds it to the array
-  for (let i = 3; i < 24; i += 4) {
-    // Stores the percentage full, available, total, and filled spots
-    let percent = garageInfo[i];
-    let avail = garageInfo[i - 2];
-
-    // If percent is greater than 0 make it visible
-    if (parseInt(percent) > 0) {
-      percentBars[pos].firstElementChild.style.visibility = 'visible';
-    }
-
-    // Sets the percent bars to their width and color
-    percentBars[pos].style.width = percent;
-    percentBars[pos].firstElementChild.innerText = percent;
-    percentBars[pos].style.background = getBarColorFromPercent(percent);
-
-    // Sets the garage icons to their color and sets their hover text
-    garages[pos].style.background = getGarageColorFromPercent(percent);
-    garageHoverText[pos].innerText = avail;
-    pos++;
-  }
-})();
